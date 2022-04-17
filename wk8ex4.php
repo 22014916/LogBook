1c7d07c27291af00b822a22c8a9c076a7c03a7f3
Abdul Salam <22014916@bucks.ac.uk>
2022-04-17 17:02:23 +0100

    changes

--- wk8ex4.php
+++ wk8ex4.php
@@ -6,7 +6,8 @@ $password = '';
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
-     if (isset($_POST['selweek'])){
+     if (isset($_POST['selweek']))
+     {
        $sql = "select * from lotto where wk=$_POST[selweek];";
        $result = mysqli_query($connect, $sql);
        $row = mysqli_fetch_array($result);
@@ -18,19 +19,24 @@ $connect = mysqli_connect($servername, $username, $password, $db_name);
        echo "Number 5 is  $row[number5]<br/>";
        echo "Number 6 is  $row[number6]<br/>";
      }
-     else {
+     else 
+      {
        $sql = "select * from lotto;";
        $result = mysqli_query($connect, $sql);
 
        echo "<form action='$_SERVER[PHP_SELF]' method='post' >";
        echo "<br/>Select the lottery week ";
        echo "<select name='selweek' >";
-       while($row = mysqli_fetch_array($result)) {
+
+       while($row = mysqli_fetch_array($result)) 
+      {
   	  echo "<option value='$row[wk]'>$row[wk]</option>";
-       }
+      }
+
        echo "</select><br/>";
        echo "<input type='submit' value='Select' />";
        echo "</form>";
-     }
+      }
+      
      mysqli_close($connect);
 ?>
